cfc73fbbfee94db1546c4c6eb3d08c52006f0057
Andrew McOlash <amcolash@gmail.com>
2026-05-26 21:16:19 -0700

    actually fix ts stuff now

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,15 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
-
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   build: {
-    outDir: join(__dirname, '../www'),
+    outDir: '../www',
     emptyOutDir: true,
   },
   resolve: {
